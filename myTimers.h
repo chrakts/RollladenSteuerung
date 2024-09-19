dc7ce11f1e75f6820396a1a5450ccee889a115b2
chrak <kaufen@cmja.de>
2024-09-19 21:10:34 +0200

    led-Blinken

--- myTimers.h
+++ myTimers.h
@@ -4,16 +4,17 @@
 #include "External.h"
 #include "myconstants.h"
 
-#define MYTIMER_NUM	3+NUM_ROLLLADEN
+#define MYTIMER_NUM	4+NUM_ROLLLADEN
 
 
-enum{TIMER_REPORT=NUM_ROLLLADEN,TIMER_ACTUAL_POSITION,TIMER_SAVE_DELAY};
+enum{TIMER_REPORT=NUM_ROLLLADEN,TIMER_ACTUAL_POSITION,TIMER_SAVE_DELAY,TIMER_LED_BLINK};
 
 
 void rollTimer(uint16_t parameter);
 void nextReportStatus(uint16_t parameter);
 void actPositionTimer(uint16_t parameter);
 void nowSaveEEProm(uint16_t parameter);
+void ledBlinken(uint16_t parameter);
 
 extern volatile TIMER2 MyTimers[MYTIMER_NUM];
 
